88e5720c8fc3ad5f3397509e71a85ded0e13057e
Sam Orgill <ssorgill@icloud.com>
2018-07-14 10:35:23 +0100

    tidy up

--- routes/goals.js
+++ routes/goals.js
@@ -1,53 +1,50 @@
 let Goal = require('../models/goal');
 
-function getGoals(req, res){
-    Goal.find( (err, goals) => {
-        if(err){
+function getGoals(req, res) {
+    Goal.find((err, goals) => {
+        if (err) {
             res.send(err)
         }
         res.send(goals);
     })
 }
 
-function getGoal(req, res){
-
+function getGoal(req, res) {
     Goal.findOne({_id: req.params._id}, (err, goal) => {
-        if(err){
+        if (err) {
             res.send(err);
         }
         res.json(goal);
     });
 }
 
-function postGoal(req, res){
+function postGoal(req, res) {
     let goal = new Goal();
     goal.name = req.body.name;
     goal.date = req.body.date;
     goal.id = req.body.id;
     goal.favorite = req.body.favorite;
 
-    goal.save( (err) => {
-        if(err){
+    goal.save((err) => {
+        if (err) {
             res.send('Cant add goal ', err);
         }
         res.json({message: `${goal.name} saved!`})
     })
 }
 
-function updateGoal(req, res){
-
+function updateGoal(req, res) {
     Goal.findByIdAndUpdate(req.body._id, {name: req.body.name}, {new: true}, (err, goal) => {
-        if(err){
+        if (err) {
             res.send(err)
         }
         res.json({message: `${goal.name} updated`})
     })
 }
 
-function deleteGoal(req, res){
-
+function deleteGoal(req, res) {
     Goal.findByIdAndDelete(req.body._id, (err, goal) => {
-        if(err){
+        if (err) {
             res.send(err);
         }
         res.json({message: `${goal.name} deleted`});
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 let goal = require('./routes/goals');
 
-
-
 mongoose.Promise = global.Promise;
 
 const uri = 'mongodb://YourFreedom:GetYourFreedom18@ds247430.mlab.com:47430/goals_tutorial';
@@ -15,15 +13,18 @@ const options = {
 };
 
 mongoose.connect(uri, options)
-    .then( () => {}, err => { console.log(err)});
+    .then(() => {
+    }, err => {
+        console.log(err)
+    });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 let port = process.env.PORT || 8010;
