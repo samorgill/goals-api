8fa08146e8c17fd1110de5cbeb2d05b943f3a9e3
Sam Orgill <ssorgill@icloud.com>
2018-07-29 13:57:40 +0100

    update

--- server.js
+++ server.js
@@ -18,10 +18,16 @@ mongoose.connect(uri, options)
         console.log(err)
     });
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//     next();
+// });
+
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     next();
 });
 
