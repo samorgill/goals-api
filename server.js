a419fde30eb55b33d3e1c1953af3a64477962f9a
Sam Orgill <ssorgill@icloud.com>
2018-07-29 13:42:38 +0100

    cors

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose.connect(uri, options)
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     next();
