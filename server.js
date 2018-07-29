f8e81bb17af77c92e23164773aeae9bbf7582bc5
Sam Orgill <ssorgill@icloud.com>
2018-07-29 15:47:42 +0100

    cors

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ mongoose.connect(uri, options)
     });
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://my-goals-app.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    // res.header("Access-Control-Allow-Methods", "PUT, DELETE");
     next();
 });
 
